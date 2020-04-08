ecd3fdd011fcf3fd5d60c5843ff552cb67004219
zhangxiaozheng <zhangxiaozheng@xiaobangtouzi.com>
2020-04-08 16:58:56 +0800

    2

--- app/background.js
+++ app/background.js
@@ -1,17 +1,11 @@
 chrome.contextMenus.create({
-  title: '测s试右键菜单',
+  title: '测试右键菜单',
   onclick: function () {
-    chrome.notifications.create(
-      12,
-      {
-        type: 'basic',
-        iconUrl: './img/kt48.png',
-        title: '这是标题',
-        message: '您刚才点击了自定义右键菜单！'
-      },
-      (res) => {
-        console.log('res', res)
-      }
-    )
+    chrome.notifications.create(null, {
+      type: 'basic',
+      iconUrl: 'img/icon.png',
+      title: '这是标题',
+      message: '1122221您刚才点击了自定义右键菜单！'
+    })
   }
 })
