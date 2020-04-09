4a6b799f5eb384eecd45ff20b1f61836e8e96d2c
zhangxiaozheng <zhangxiaozheng@xiaobangtouzi.com>
2020-04-09 11:15:37 +0800

    6

--- 😈实用/复制插件/content-script-jianshu.js
+++ 😈实用/复制插件/content-script-jianshu.js
@@ -1,20 +1,3 @@
-;(function () {
-  console.log('content-script ---- www.jianshu.com!!')
-})()
-
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  console.log('message: ', message)
-  if (message.method === 'tab') {
-    console.log(message.message)
-  }
-})
-
-// 登录过简书才会种下这个local
-const value = 'www.jianshu.com'
-chrome.storage.local.set({ thisWebsite: value }, function () {
-  console.log(thisWebsite, ' is set to ' + value)
-})
-
 function copyText(text, callback) {
   // text: 要复制的内容， callback: 回调
   var tag = document.createElement('input')
