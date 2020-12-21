392da09a611b93b286cec1aa7f0139a11545102d
zhangxiaozheng <xiaozheng.zhang@niu.com>
2020-12-21 09:50:12 +0800

    1

---
+++ 😈实用/视频下载插件1.8/content-script.js
@@ -0,0 +1,39 @@
+﻿function Func() {
+	return new Promise((resolve, reject) => {
+		// var a = document.querySelector("#player >script:nth-child(1)").innerHTML
+		var a = document.querySelector("#player >script:nth-child(2)")
+		if (!a) {
+			a = document.querySelector("#player >script:nth-child(1)")
+		}
+		a = a.innerHTML
+		a = `	var playerObjList = {};\n${a}`
+		var c = a.match("flashvars_[0-9]{1,}")[0]
+		eval(a)
+		var d = eval(c)
+		resolve(d)
+		console.log(d)
+	})
+}
+
+Func().then(res => {
+	var videoType = []
+	Object.keys(res['mediaDefinitions']).forEach((item) => {
+		var itemInfo = res['mediaDefinitions'][item]
+		if (itemInfo['format'] == 'mp4') {
+			console.log(itemInfo)
+			var obj = {
+				key: itemInfo['quality'],
+				val: itemInfo['videoUrl'],
+				video_title:res.video_title
+			}
+			videoType.push(obj)
+		}
+	})
+	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		if (request.cmd == 'test') 
+			sendResponse(videoType);
+	});
+})
+
+
+
